758ba44242e4566b82afdda7bd0bd19576221a34
Albert Ivanov <albertivanov187@gmail.com>
2024-12-05 22:55:06 +0200

    ArrayOfMultiples

---
+++ ArrayOfMultiples/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+void ArrayOfMultiples(int num, int length)
+{
+	int[] array = new int[length];
+
+	array[0] = num;
+
+	int nextNumber = num;
+
+	for (int i = 1; i <= array.Length - 1; i++)
+	{
+		array[i] = nextNumber += num;
+		nextNumber = array[i];
+	}
+
+    Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+ArrayOfMultiples(17, 6);
