a7dbee850716b2dbc89dd823bbadf6f0143cc455
Albert Ivanov <albertivanov187@gmail.com>
2024-12-05 23:10:05 +0200

    ReverseTheCase

---
+++ ReverseTheCase/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+void ReverseTheCase(string input)
+{
+	for (int i = 0; i < input.Length; i++)
+	{
+		if (input[i].ToString() == input[i].ToString().ToUpper())
+		{
+			Console.Write(input[i].ToString().ToLower());
+		}
+		else if (input[i].ToString() == input[i].ToString().ToLower())
+		{
+			Console.Write(input[i].ToString().ToUpper());
+		}
+	}
+}
+
+ReverseTheCase("sPoNtAnEoUs");
