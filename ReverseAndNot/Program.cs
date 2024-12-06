557e36bed009044984e416028a5df9ea47f820f7
Albert Ivanov <albertivanov187@gmail.com>
2024-12-06 15:12:01 +0200

    ReverseAndNot

---
+++ ReverseAndNot/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+using System.Linq;
+
+void ReverseAndNot(int number)
+{
+	string result = number.ToString();
+	char[] chars = result.ToCharArray();
+
+	Array.Reverse(chars);
+
+    Console.WriteLine(string.Join("", chars) + result);
+}
+
+ReverseAndNot(123456789);
